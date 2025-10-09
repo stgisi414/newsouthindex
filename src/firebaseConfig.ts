7eb897a49e9e4fea95537363a2916a637d7ebb30
Stefan Gisi <141892560+stgisi414@users.noreply.github.com>
2025-10-09 16:10:30 -0500

    remove app check

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
-import { getAppCheck, initializeAppCheck, ReCaptchaEnterpriseProvider } from "firebase/app-check"; 
 
 // Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
@@ -17,18 +16,7 @@ const firebaseConfig = {
 // Initialize Firebase ONCE
 const app = initializeApp(firebaseConfig);
 
-if (import.meta.env.VITE_RECAPTCHA_SITE_KEY) {
-  initializeAppCheck(app, {
-    // You must use ReCaptchaEnterpriseProvider for the highest security level
-    provider: new ReCaptchaEnterpriseProvider(
-      import.meta.env.VITE_RECAPTCHA_SITE_KEY as string
-    ),
-    is
-  });
-}
-
 // Export the instances of the services for the rest of your app to use
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const functions = getFunctions(app);
-export const appCheck = getAppCheck(app);
