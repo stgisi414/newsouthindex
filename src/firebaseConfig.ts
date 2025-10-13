686a69acbc0a5daa54f77169b5600f22a6577449
Stefan Gisi <141892560+stgisi414@users.noreply.github.com>
2025-10-12 21:27:27 -0500

    stuff

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -24,6 +24,6 @@ export const functions = getFunctions(app);
 // ADD THIS SECTION TO CONNECT TO EMULATORS
 if (import.meta.env.DEV) {
   connectAuthEmulator(auth, "http://127.0.0.1:9099");
-  connectFirestoreEmulator(db, "127.0.0.1", 5001);
+  connectFirestoreEmulator(db, "127.0.0.1", 8080);
   connectFunctionsEmulator(functions, "127.0.0.1", 5003);
 }
